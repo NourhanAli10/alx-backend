249bb55d74ce48ab78c8ed83ed6dba0e5b8e091e
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-05-29 01:55:48 +0300

    tasks

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+""" Basic Flask app module.
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """ Hello method"""
     return render_template('index.html')
 
 if __name__ == '__main__':
